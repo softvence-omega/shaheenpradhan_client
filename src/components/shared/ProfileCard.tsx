0c5fe1bb147e0b709db2190c20493ca8b330e520
kazimehedihasan1 <mehedihasanarabi00@gmail.com>
2025-05-24 10:22:33 +0600

    card complete

---
+++ src/components/shared/ProfileCard.tsx
@@ -0,0 +1,10 @@
+const ProfileCard = () => {
+  return <div>
+    {/* PROFILE CARD STARING */}
+    <div className="px-4 rounded-xl">
+d
+    </div>
+  </div>;
+};
+
+export default ProfileCard;
--- src/pages/Home/HomePage.tsx
+++ src/pages/Home/HomePage.tsx
@@ -1,13 +1,14 @@
-
 import Filtering from "@/components/shared/Filter/Filtering";
 import Review from "@/components/Home/Review";
 import BookingCard from "@/components/shared/BookingCard";
+import ProfileCard from "@/components/shared/ProfileCard";
 
 const HomePage = () => {
   return (
     <div>
       <Filtering />
       <BookingCard />
+      <ProfileCard />
       <Review />
     </div>
   );
